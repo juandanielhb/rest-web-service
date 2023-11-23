fe61f4c31b282988d17e53aed64a137951a08661
Juan D Hernandez <juandanielhb@gmail.com>
2023-11-22 21:40:41 -0500

    Initial commit - Spring base project

---
+++ src/main/java/com/jdhb/rest_web_service/RESTWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jdhb.rest_web_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RESTWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RESTWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jdhb/rest_web_service/RESTWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdhb.rest_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RESTWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
